d51a7d47deee2d98a212a9ac226243289eff139d
nicolas borba <borbanicolasmk@gmail.com>
2022-01-20 13:45:09 -0300

    display function

--- JS/cripto.js
+++ JS/cripto.js
@@ -1,9 +1,9 @@
 const btn = document.querySelector("#send")
-
+let TextoEscrito
 
 btn.addEventListener("click", function(e){
     e.preventDefault()
     const name = document.querySelector("#name")
-    const value = name.value 
-    console.log(value)
+    TextoEscrito = name.value 
+    return TextoEscrito
 })

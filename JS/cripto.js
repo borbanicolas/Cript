e1cf89a97970ae063a00cd2ffbdaa7a0f30a1a8e
nicolas borba <borbanicolasmk@gmail.com>
2022-01-20 13:48:06 -0300

    function display

--- JS/cripto.js
+++ JS/cripto.js
@@ -1,9 +1,33 @@
 const btn = document.querySelector("#send")
 let TextoEscrito
+let Resultado
+let textoCriptografado
+let cifrador = document.getElementById('.box2')
+cifrador.innerText = "texto"
 
-btn.addEventListener("click", function(e){
+title.textContent = "teste"
+ btn.addEventListener("click", function(e){
     e.preventDefault()
     const name = document.querySelector("#name")
-    TextoEscrito = name.value 
-    return TextoEscrito
+    TextoEscrito = name.value
+    Resultado = criptografar(TextoEscrito)
+    console.log(Resultado)
+   
+    return Resultado
 })
+const validarCriptografia = (texto) => {
+    return /^[a-z\s]*$/.test(texto)
+  };
+
+const criptografar = (str) => {
+    if (validarCriptografia(str)) {
+      return  str
+      .replaceAll("e", "enter")
+      .replaceAll("i", "imes")
+      .replaceAll("a", "ai")
+      .replaceAll("o", "ober")
+      .replaceAll("u", "ufat")
+    }
+    return false;
+  };
+

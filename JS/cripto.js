4a418380b128dcbec458ab45b17931ec1109078e
nicolas borba <borbanicolasmk@gmail.com>
2022-01-20 17:30:14 -0300

    function

--- JS/cripto.js
+++ JS/cripto.js
@@ -2,19 +2,18 @@ const btn = document.querySelector("#send")
 let TextoEscrito
 let Resultado
 let textoCriptografado
-let cifrador = document.getElementById('.box2')
-cifrador.innerText = "texto"
 
-title.textContent = "teste"
  btn.addEventListener("click", function(e){
     e.preventDefault()
     const name = document.querySelector("#name")
     TextoEscrito = name.value
     Resultado = criptografar(TextoEscrito)
-    console.log(Resultado)
-   
+     console.log(Resultado)
     return Resultado
 })
+
+
+
 const validarCriptografia = (texto) => {
     return /^[a-z\s]*$/.test(texto)
   };
